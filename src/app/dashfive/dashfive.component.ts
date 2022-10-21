addd381d678fd249a7ae0a730d4ede186832ee6c
mm180601 <milanovic.milica1999@gmail.com>
2022-10-21 15:31:25 +0200

    commit message

--- src/app/dashfive/dashfive.component.ts
+++ src/app/dashfive/dashfive.component.ts
@@ -172,6 +172,8 @@ message:string;
     if(event.key=="]")
   {
     this.word+=event.key;
+    if(this.word.charAt(this.word.length-2)=="]")
+    this.word=this.word.slice(0,-1);
     this.cassette=this.word
     this.word=""
   }
