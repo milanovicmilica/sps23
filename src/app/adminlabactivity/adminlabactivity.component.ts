39ca2de3f7d634cef87e4c3e8e38520b77c63aa5
mm180601 <milanovic.milica1999@gmail.com>
2022-11-18 18:26:35 +0100

    Commit message

--- src/app/adminlabactivity/adminlabactivity.component.ts
+++ src/app/adminlabactivity/adminlabactivity.component.ts
@@ -20,7 +20,7 @@ export class AdminlabactivityComponent implements OnInit {
   constructor(private router: Router, private UserService: UserService) { }
   canvas: any;
   ctx: any;
-  @ViewChild('mychart') mychart:any;
+  @ViewChild('myChart') mychart:any;
   ngOnInit(): void {
     let date=new Date();
     let dan=new Date().getDate();
