ad3999ab74547198acf47ffb869285595e9c6401
mm180601 <milanovic.milica1999@gmail.com>
2022-11-28 01:06:59 +0100

    first commit

--- src/app/archivedash/archivedash.component.ts
+++ src/app/archivedash/archivedash.component.ts
@@ -1,5 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,HostListener } from '@angular/core';
+import { User } from '../models/user';
+import { Router } from '@angular/router';
+import { Process } from '../models/process';
+import { Processor } from '../models/processors';
 
+import { UserService } from '../user.service';
+import { Sample } from '../models/sample';
+import { Case } from '../models/case';
+import { Cs } from '../models/infocs';
+import { Embedding } from '../models/embedding';
 @Component({
   selector: 'app-archivedash',
   templateUrl: './archivedash.component.html',
@@ -7,9 +16,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArchivedashComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private UserService: UserService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {  let user1 = JSON.parse(sessionStorage.getItem("laborant")) as User; 
+  this.me=user1;
   }
-
+  me:User;
+  logout(){
+    sessionStorage.clear();
+    this.router.navigate(['/login-archive']);
+  }
+  inputval:string;
+  
 }
